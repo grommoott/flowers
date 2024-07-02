34ce46b2280e5bd94e5a3682170bb05c3aeb2c42
grommoott <grommoott@gmail.com>
2024-07-03 02:03:28 +0700

    done!

--- server/app.js
+++ server/app.js
@@ -1,9 +1,7 @@
 const express = require("express")
 const app = express()
 const port = process.env.PORT ? process.env.PORT : 7777
-const cors = require("cors")
 
 app.use(express.static("../dist"))
-app.use(cors())
 
-app.listen(7777)
+app.listen(port)
